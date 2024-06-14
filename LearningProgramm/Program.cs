069521fef147cbc05672eff9bf3a3b75bb5afaa9
GeChao <2643141622@qq.com>
2024-06-14 19:23:36 +0800

    添加项目文件。

---
+++ LearningProgramm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningProgramm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
